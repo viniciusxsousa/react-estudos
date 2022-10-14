6d9c98f964c1902319d91d094015267c5d13f4e6
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-10-14 16:45:22 -0300

    aplicando css com styled components

--- app/src/components/Pessoa/index.jsx
+++ app/src/components/Pessoa/index.jsx
@@ -1,10 +1,12 @@
+import { Conteiner } from './styles'
+
 function Pessoa(props){
     return(
-        <div>
+        <Conteiner>
             <img src={props.foto} alt={props.nome}></img>
             <h3>Nome: {props.nome}</h3>
             <p>Idade: {props.idade}</p>
-        </div>
+        </Conteiner>
     )
 }
 
---
+++ app/src/components/Pessoa/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Conteiner = styled.div`
+    width: 200px;
+    margin: auto;
+    padding-top: 50px;
+
+    p{
+        color: red;
+    }
+`
