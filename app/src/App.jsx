4ee6b875c0ed7e6d3df85602ffacc6300f9afb52
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-10-13 22:38:53 -0300

    aula de componentes

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import HelloWord from './components/HelloWord'
+
+function App() {
+  return (
+    <div>
+      <h1>Meu app porra</h1>
+      <HelloWord/>
+    </div>
+  )
+}
+
+export default App
