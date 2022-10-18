62218f7be4515f1f3afa34f0a972f0b0c90ff309
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-10-18 14:20:31 -0300

    utilizando useState

--- 00-app/src/App.jsx
+++ 00-app/src/App.jsx
@@ -3,6 +3,7 @@ import EventoForms from "./components/EventoForms"
 function App() {
   return (
     <>
+      <h1>Testando Use State</h1>
       <EventoForms></EventoForms>
     </>
   )
--- 00-app/src/components/EventoForms/index.jsx
+++ 00-app/src/components/EventoForms/index.jsx
@@ -1,18 +1,42 @@
+import { useState } from 'react'
+
 function EventoForms(){
 
-    function envio(e){
+    const [user, setUser] = useState();
+    const [senha, setSenha] = useState();
+
+    function cadastrar(e){
         e.preventDefault();
-        console.log('olá');
+        console.log(`Usuário: ${user}`);
+        console.log(`Senha: ${senha}`);
     }
 
     return (
-        <form onSubmit={envio}>
+        <form onSubmit={cadastrar}>
+
             <div>
-                <input type="text"  placeholder="Insira um texto"/>
+                <label htmlFor="user">Usuário: </label>
+                <input 
+                    type="text" 
+                    name="user" 
+                    id="user" 
+                    placeholder="Digite seu usuário" 
+                    onChange={(e) => setUser(e.target.value)}
+                />
             </div>
+
             <div>
-                <input type="submit" value="Enviar" />
+                <label htmlFor="pass">Senha: </label>
+                <input 
+                    type="password" 
+                    id="pass" 
+                    name="pass" 
+                    placeholder="Digite sua senha: "
+                    onChange={(e) => setSenha(e.target.value)}
+                />
             </div>
+
+            <input type="submit" value="Cadastrar" />
         </form>
     )
 }
